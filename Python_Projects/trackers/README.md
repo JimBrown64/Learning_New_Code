ded71333ee37111c81a743048ab59b0976b2d560
JimBrown64 <98353301+JimBrown64@users.noreply.github.com>
2024-04-04 13:47:59 -0400

    added code notes, did additional testing to ensure functionality, made small adjustments to fix issues that came up during testing

--- Python_Projects/trackers/README.md
+++ Python_Projects/trackers/README.md
@@ -1,5 +1,5 @@
-The files here are all a part of the development of a tracker that basically is an alternative to the use of a 
-    spreadsheet. The intent here was to build skills in the use of Python, and by extension tkinter.
+The files here are all a part of the development of a tracker that at its core is an easy-to-use 
+    spreadsheet alternative. The intent here was to build skills in the use of Python and by extension, tkinter.
 
 date_management -  Due to a lack of a date data-type in sqlite3, this module was built to handle validating date formats,
     and formatting dates for ease of data storing, and allowing the proper filtering and sorting of date data. Currently planning to move this to a separate directory, so it can be accessed easily from other applications.
@@ -12,8 +12,7 @@ sql_interactions - A collection of functions intended to handle database operati
 tracker_base - A module that contains the core functions to build the application. It has been built to allow variants of 
     the app to be created by creating a small file containing a few variables, and executions of functions located here.
 
-tracker_XXXX - specific uses of the tracker base. Will be removed from directory at a later time. *NO LIVE DATA IS STORED IN
-    STRAY DB FILES
+tracker_XXXX - specific uses of the tracker base. Will be removed from directory at a later time. *NO LIVE DATA IS STORED IN STRAY DB FILES
 
 utilities - future home of data_management and sql_interactions, once it can be pegged down how to get the calls to their
-    modules to work correctly. Once the issue has been addressed, the plan is to move this directory outside of the trackers directory to allow access from other apps to be made in the future.
+    modules to work correctly. Once the issue has been addressed, the plan is to move this directory outside of the trackers directory to allow access from other apps in the future.
--- Python_Projects/trackers/date_management.py
+++ Python_Projects/trackers/date_management.py
@@ -7,16 +7,16 @@ year = today.year
 
 
 
-def validateDate(date): #used to verify a date is passed in the desired format. returns 1 or 0
+def validateDate(date): #used to verify a date is passed in the desired format. returns 1 for succ, or 0 for fail
     try:
         resultDate = str(date).split("/")   #take the date, convert it to a string, and turn into an array separated by "/"
-        outcome = 1                         #used to track if there is a failure during validation. 1 = success, 0 = fail
+        outcome = 1                         #used to track if there is a failure during validation. 
         def validateFormat(resultDate,date): 
             check = 0
             if re.search("../../....",date) is not None: #checks the date passed is in the specified pattern
                 check = 1                   #passes if pattern matches
             else:   
-                check = 0                   #fails if pattern does not match
+                check = 0                   
                 return(check) 
             if \
                 resultDate[0].isnumeric() is True and \
@@ -24,7 +24,7 @@ def validateDate(date): #used to verify a date is passed in the desired format. 
                 resultDate[2].isnumeric() is True:
                     check = 1               #varifies that all values in the date are numbers, if yes passes
             else:
-                check = 0                   #if all values are not numbers, fails
+                check = 0                   #if any values are not numbers, fails
             return(check)
         
         def validateMonth(resultDate):
@@ -34,7 +34,7 @@ def validateDate(date): #used to verify a date is passed in the desired format. 
                 int(resultDate[0]) >= 1:    #checks for a valid month range. 
                 check = 1                   #passes if in range
             else:
-                check = 0                   #fails if out of range
+                check = 0                   
             return(check)
                 
         def validateDay(resultDate):        #confirms the day is valid for the given month/year
@@ -42,7 +42,7 @@ def validateDate(date): #used to verify a date is passed in the desired format. 
             check = dayRange(int(resultDate[2]),int(resultDate[0]),int(resultDate[1]))
             return(check)
         
-        def validateYear(resultDate):       #confirms the year is a valid, existing year.
+        def validateYear(resultDate):       #confirms the year is a valid, existing year, setting 1900 as the minimum
             check = 1
             if  \
                 int(resultDate[2]) >= 1900 and \
@@ -87,7 +87,7 @@ def tableFormat(date): #reformats the currently accepted date to an easy-to-use 
     for item in splitDate:
         output = output + item + '/'
     output = output[:-1] #above for loop adds an additional / at the end, this removes it
-    return output #outputs date in YYYYMMDD format
+    return output #outputs date in YYYY/MM/DD format, to make sorting easier
 
 def displayFormat(date): #reformats table data to an easy to read version.
     splitDate = date.split("/")
