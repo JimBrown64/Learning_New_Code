19c5cc5012afd1386cd2df003615733c9c2fd9f4
JimBrown64 <jimmybrown1243@gmail.com>
2024-11-19 14:36:37 -0500

    small update

--- Java_Projects/javadatadisplay/DataDisplay.java
+++ Java_Projects/javadatadisplay/DataDisplay.java
@@ -1,9 +1,14 @@
 package Java_Projects.javadatadisplay;
-
+import java.sql.*;
 
 public class DataDisplay{
 
     public static void main(String[] args){
         System.out.println("hello, my dude");
+
+        try{
+            Class.forName("org.sqlite.JDBC");
+            
+        }
     }
 }
